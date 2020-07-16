ffad83760cfa245cad4514eac4b7eb0f11066c2e
Hiren Mistry <hirenmst@gmail.com>
2020-07-16 02:00:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,27 +1,24 @@
 def line(array)
-  if array.length >= 1
-    new_array = []
-    counter = 1 
-    array.each do |name|
-      new_array.push("#{counter}. #{name}")
-      counter += 1 
-    end 
-    puts "The line is currently: #{new_array.join(" ")}"
-  else
+   current_line = "The line is currently:"
+  
+  if array.length == 0 
     puts "The line is currently empty."
   end
+  
+  if array.length > 0 
+    array.each_with_index do |person, idx|
+      current_line << " #{idx + 1}. #{person}"
+    end
+    puts current_line
+  end
+  
+  
 end
 
 def take_a_number(line, new_person)
-   line << new_person
-   puts "Welcome, #{new_person}. You are number #{line.length} in line."
+   
 end
 
 def now_serving(line)
-  if line.length == 0
-    puts"There is nobody waiting to be served!"
-  else
-    puts "Currently serving #{line[0]}."
-    line.shift
-  end
+  
 end
